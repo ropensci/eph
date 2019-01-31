a90e2baef0ad31aceb1bcc0b494ed79fa57bb26b
Pablo <30708601+pablinte@users.noreply.github.com>
2019-01-31 10:50:09 -0300

    Update base_descarga.r

--- R/base_descarga.r
+++ R/base_descarga.r
@@ -13,26 +13,27 @@ descarga_base <- function(base = 'EPH_usu_2_Trim_2017_txt.zip', individual = FAL
   base_hogar_name <- nombres[str_detect(nombres, 'hog')]
   base_individual_name <- nombres[str_detect(nombres, 'ind')]
   
-  if (individual = TRUE & etiqueta = TRUE) {
+  if (individual == TRUE) {
+    if (etiqueta == TRUE) {
     base_individual <<- read.table(unz(temp,base_individual_name), sep=";", dec=",", header = TRUE, fill = TRUE)
     source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_ind_2018.R", encoding="UTF-8")
-      } else {
-      base_individual <<- read.table(unz(temp,base_individual_name), sep=";", dec=",", header = TRUE, fill = TRUE)
-      }
-       
-  if (hogar = TRUE & etiqueta = TRUE) {
+  } else {
+    base_individual <<- read.table(unz(temp,base_individual_name), sep=";", dec=",", header = TRUE, fill = TRUE)
+  }
+  }
+  
+  if (hogar == TRUE) {
+    if (etiqueta == TRUE) {
     base_hogar <<- ?read.table(unz(temp,base_hogar_name), sep=";", dec=",", header = TRUE, fill = TRUE)
     source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_hog_2018.R", encoding="UTF-8")
   } else {
     base_hogar <<- ?read.table(unz(temp,base_hogar_name), sep=";", dec=",", header = TRUE, fill = TRUE)
-    }
+  }
+  }
   
   unlink(temp)
-
+  
 }
-
-
-
 
 
 
