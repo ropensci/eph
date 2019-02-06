9827fddbc8a7362d71d47381692eca40d3ccc0c2
DiegoKoz <diegokoz92@gmail.com>
2019-02-06 16:20:28 -0300

    add first function. descarga bases.

--- R/descarga_base_eph.r
+++ R/descarga_base_eph.r
@@ -1,16 +1,29 @@
-### ADVERTENCIA: El script no es oficial, pero descarga las bases desde la página oficial (publicadas). 
-### El primer uso puede tomarse un tiempito (dependiendo tu compu / conexión) para instalar aquellos paquetes que aún no 
-# tengas instalados. El segundo uso apenas tomará unos segundos.
+#'Función que descarga bases de la página de indec, según el nombre que le pusieron al zip.
+#'@description
+#' El primer uso puede tomarse un tiempito (dependiendo tu compu / conexión) para instalar aquellos paquetes que aún no
+#'tengas instalados. El segundo uso apenas tomará unos segundos.
+#'
+#'@param
+#'anio: un integer entre 2016:2018
+#'trimestr: un integer con el numero de trimestre: 1,2,3,4
+#'etiqueta: TRUE/FALSE, opcion para etiquetar los datos
+#'@details
+#'disclaimer: El script no es un producto oficial de INDEC.
+#'
+#'@example
+#'descarga_base_eph(anio = 2018, trimestre = 1, etiqueta = FALSE)
+#'
+descarga_base_eph <- function(anio = 2018, trimestre = 1,etiqueta = FALSE){
 
-# Función que descarga bases de la página de indec, según el nombre que le pusieron al zip.
-descarga_base_eph <- function(anio = 2018, trimestre = 1, individual = FALSE, hogar = FALSE,
-                          etiqueta = FALSE){
-  
-### Chequea si el paquete pacman está instalado, en caso de no estarlo, lo instala.
-if(!require(pacman))install.packages("pacman")
-### El paquete 'pacman' carga e instala (si no está aún instalado) el paquete que indicamos en la función 'p_load'.
-pacman::p_load(tidyverse, expss, httr, stringr, haven, readxl)
-  
+  #controles de los parametros
+  assertthat::assert_that(is.numeric(anio))
+  assertthat::assert_that(is.numeric(trimestre))
+  assertthat::assert_that(assertthat::is.flag(etiqueta), msg = "Por favor ingresa TRUE o FALSE")
+  assertthat::assert_that(trimestre %in% 1:4, msg = "Por favor ingresa un numero de trimeste valido: 1,2,3,4")
+
+  assertthat::assert_that(anio>=2016,msg = "Esta funcion solo es valida para las bases a partir de 2016")
+
+
   if (anio == 2016 | (anio == 2017 & trimestre ==1)){
     if (trimestre == 1){
       trimestre <- "1ro"}
@@ -20,35 +33,36 @@ pacman::p_load(tidyverse, expss, httr, stringr, haven, readxl)
       trimestre <- "3er"}
     if (trimestre == 4){
       trimestre <- "4to"}
-    
+
   base = paste0("EPH_usu_", trimestre, "Trim_", anio, "_txt.zip")
   }
-  
+
   if (anio >= 2018 | (anio == 2017 & trimestre >=2)){
   base = paste0("EPH_usu_", trimestre, "_Trim_", anio, "_txt.zip")
   }
-  
+
   link = paste0('https://www.indec.gob.ar/ftp/cuadros/menusuperior/eph/', base)
   temp <- tempfile()
-  download.file(link,temp)
-  nombres <- as_vector(unzip(temp, list = TRUE)['Name'])
+
+  check <- NA
+  try(check <- download.file(link,temp),silent = TRUE)
+  assertthat::assert_that(assertthat::noNA(check),msg = "problema con la descarga. Posiblemente un error de la conexion a internet")
+
+  nombres <- purrr::as_vector(unzip(temp, list = TRUE)['Name'])
   base_hogar_name <- nombres[str_detect(nombres, 'hog')]
   base_individual_name <- nombres[str_detect(nombres, 'ind')]
-  
-  if (individual == TRUE) {
-    base_individual <<- read.table(unz(temp,base_individual_name), sep=";", dec=",", header = TRUE, fill = TRUE)
-    if (etiqueta == TRUE) {
-      source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_ind_2018.R", encoding="UTF-8")
-    } 
+
+  base_individual <- read.table(unz(temp,base_individual_name), sep=";", dec=",", header = TRUE, fill = TRUE)
+  if (etiqueta == TRUE) {
+    source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_ind_2018.R", encoding="UTF-8")
   }
-  
-  if (hogar == TRUE) {
-    base_hogar <<- read.table(unz(temp,base_hogar_name), sep=";", dec=",", header = TRUE, fill = TRUE)
-    if (etiqueta == TRUE) {
-      source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_hog_2018.R", encoding="UTF-8")
-    } 
+
+  base_hogar <- read.table(unz(temp,base_hogar_name), sep=";", dec=",", header = TRUE, fill = TRUE)
+  if (etiqueta == TRUE) {
+    source("https://github.com/pablinte/eph/raw/master/R/etiquetas_eph_hog_2018.R", encoding="UTF-8")
   }
-  
+
   unlink(temp)
-  
+  bases <- list("base_hogar"=base_hogar,"base_individual"=base_individual)
+
 }
--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# EPH
+# EPH en R
 
-### Herramientas varias destinadas a facilitar el procesamiento de los datos producidos por la Encuesta Permanente de Hogares, relevada por el [INDEC](https://www.indec.gob.ar/) trimestralmente.
+## Descarga de base usuaria EPH-INDEC
+Las bases usuarias de la EPH-INDEC son gratuiras y de libre acceso. Para disponer de las mismas hay que recurir a la [página del INDEC](https://www.indec.gob.ar/bases-de-datos.asp) y bajar, una por una, las bases que querramos.
+
+Con el objetivo de facilitar este procedimiento e integrarlo directamente al procesamiento desde R, con [Natsu](https://rpubs.com/natsumi_shokida) y [@DiegoKoz](https://github.com/DiegoKoz) creamos una función que permita descargar de forma automática la base usuaria, sea la de individuos o la de hogares.
+
+A su vez, integramos en la misma función de descarga la opción para etiquetar los nombres de las variables y también el de sus  categorías.
+
+### Modo de uso.
+Se puede copiar el script de la función desde [este repositorio](https://github.com/pablinte/eph/raw/master/R/descarga_base_eph.r) o simplemente pegar el siguiente código en R --> `source("https://github.com/pablinte/eph/raw/master/R/base_descarga.r")`. 
+
+Al correrlo automaticamente queda cargada la función en tu ambiente de R. Para descargar la base hay que definir en la función los siguientes parámetros:
+`anio = `,
+`trimestre =`,
+`individual = TRUE/FALSE`,
+`hogar = TRUE/FALSE`,
+`etiqueta = TRUE/FALSE`.
+
+### Ejemplos.
+Si quiero descargar la base de individuos de la EPH, para el año 2018, trimestre 1, el código quedaría así:
+`descarga_base_eph(individual = TRUE, etiqueta = TRUE, anio = 2018, trimestre = 1)`
+
+![alt text](https://raw.githubusercontent.com/pablinte/eph/master/R/imagenes/capt3.PNG)
+
+Con la función table chequeamos que se haya descargado el año, trimestre y las etiquetas:
+`table(base_individual$ANO4, base_individual$TRIMESTRE)`
+
+![alt text](https://raw.githubusercontent.com/pablinte/eph/master/R/imagenes/table.PNG)
 
 
-* [**Scripts para R**](https://github.com/pablinte/eph/tree/master/R). 
-
-* [**Sintaxis para SPSS**](https://github.com/pablinte/eph/tree/master/spss).
+## Etiquetas
+Si ya dispones de una base de datos y sólo querés atiquetarla, podés correr el siguiente código:
+`table(base_individual$ANO4, base_individual$TRIMESTRE)`
