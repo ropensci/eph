7ca8b03de479e4390f1e66269f899f01afa96e05
DiegoKoz <diegokoz92@gmail.com>
2019-03-01 11:25:06 -0300

    add warnings

--- R/get_microdata.R
+++ R/get_microdata.R
@@ -22,6 +22,22 @@ get_microdata <- function(year = 2018, trimester = 1,type='individual',labels = 
     assertthat::assert_that(is.numeric(trimester))
     assertthat::assert_that(assertthat::is.flag(labels), msg = "Por favor ingresa TRUE o FALSE")
     assertthat::assert_that(trimester %in% 1:4, msg = "Por favor ingresa un numero de trimeste valido: 1,2,3,4")
+    assertthat::assert_that(type %in% c('individual','hogar'))
+    assertthat::assert_that(!(year==2007 & trimester==3), msg="INDEC advierte: La información correspondiente al tercer trimestre 2007 no está disponible ya que los aglomerados Mar del Plata-Batán, Bahía Blanca-Cerri y Gran La Plata no fueron relevados por causas de orden administrativo, mientras que los datos correspondientes al Aglomerado Gran Buenos Aires no fueron relevados por paro del personal de la EPH.")
+    assertthat::assert_that(!((year==2015 & trimester %in% 3:4)|(year==2016 & trimester ==1)), msg="En el marco de la emergencia estadística el INDEC no publicó la base solicitada.
+                            más informacón en: https://www.indec.gob.ar/ftp/cuadros/sociedad/anexo_informe_eph_23_08_16.pdf")
+
+    if (year %in% 2007:2015) {
+      warning("INDEC advierte:
+'''
+              Advertencia sobre el uso de series estadísticas
+
+Se advierte que las series estadísticas publicadas con posterioridad a enero 2007 y hasta diciembre 2015 deben ser consideradas con reservas, excepto las que ya hayan sido revisadas en 2016 y su difusión lo consigne expresamente. El INDEC, en el marco de las atribuciones conferidas por los decretos 181/15 y 55/16, dispuso las investigaciones requeridas para establecer la regularidad de procedimientos de obtención de datos, su procesamiento, elaboración de indicadores y difusión.
+'''
+más informacón en: https://www.indec.gob.ar/ftp/cuadros/sociedad/anexo_informe_eph_23_08_16.pdf
+")
+    }
+
 
     link = glue::glue('https://github.com/rindec/data/raw/master/eph/{type}/base_{type}_{year}T{trimester}.RDS')
 
