34608b1c39d4a15e31d444a5657f79bee65f62ed
DiegoKoz <diegokoz92@gmail.com>
2019-05-19 17:14:10 +0200

    add test organize labels individual. add .travis.yml to Rbuildignore

--- tests/testthat/test-organize_labels.R
+++ tests/testthat/test-organize_labels.R
@@ -1,8 +1,16 @@
 context("test-organize_labels")
 
-test_that("multiplication works", {
+test_that("labels hogar", {
   df <- get_microdata(year = 2018, trimester = 1,type='hogar')
   df <- organize_labels(df, type='hogar')
+  regiones <- expss::val_lab(df$REGION)
+  expect_equal(!is.null(regiones),TRUE)
+})
+
+
+test_that("labels individual", {
+  df <- get_microdata(year = 2018, trimester = 1,type='individual')
+  df <- organize_labels(df, type='individual')
   regiones <- expss::val_lab(df$REGION)
   expect_equal(!is.null(regiones),TRUE)
 })
