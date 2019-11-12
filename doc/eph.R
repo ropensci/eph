523a688fcbfe98f18b8134f84cbf06d89a40aadd
gefero <german.rosati@gmail.com>
2019-11-12 01:46:07 -0300

    Add vignette

--- doc/eph.R
+++ doc/eph.R
@@ -47,7 +47,7 @@ bases <- list(i_2018_1=get_microdata(year=2018, trimester=1, type='individual'),
 
 ## ------------------------------------------------------------------------
 pool <- organize_panels(bases=bases, variables=c('ESTADO'),
-                        window='trimestral')
+                        window='anual')
 
 ## ------------------------------------------------------------------------
 pool
@@ -55,47 +55,103 @@ pool
 ## ------------------------------------------------------------------------
 
 
-## ------------------------------------------------------------------------
-years <- 2011:2018
-trims <- 1:4
+## ----message=FALSE, warning=FALSE----------------------------------------
+year <- 2004:2018
+trimester <- 1:4
+wave<-NA
+type<-'individual'
 
-indicator<-matrix(nrow=length(years) * length(trims), ncol=3)
-i<-0
-for (y in years){
-        for (t in trims){
-                i<-i+1
-                d<-get_microdata(year=y, trimester = t) %>%
-                  organize_labels(.)
+df <- as_tibble(expand.grid(year, trimester, wave, type))
+colnames(df)<-c('year','trimester','wave', 'type')
+
+get_data <- function(year, trimester, wave=NA, type){
+  indicator<-list()  
+  d <- get_microdata(year=year, 
+                     trimester=trimester, 
+                     type=type, 
+                     wave=wave)
+
+  indicator$year<-year
+  indicator$trimester<-trimester
+
+  if (nrow(d) == 0){ 
+    indicator$indic<-NA
+  }
                 
-                indicator[i,1]<-y
-                indicator[i,2]<-t
+  if (year==2007 & (trimester ==2 | trimester == 4)) {
+    indicator$ind<-sum(d[d$cat_ocup==2,]$pondera, na.rm=TRUE) /sum(d[d$estado==1,]$pondera, na.rm=TRUE)
+  }
                 
-                if (nrow(d) == 0){ 
-                  indicator[i,3]<-NA
-                }
-                
-                if (y==2007 & (t ==2 | t == 4)) {
-                  indicator[i,3]<-sum(d[d$cat_ocup==2,]$pondera, na.rm=TRUE) /sum(d[d$estado==1,]$pondera, na.rm=TRUE)
-                }
-                
-                else {
-                indicator[i,3]<-sum(d[d$CAT_OCUP==2,]$PONDERA, na.rm=TRUE) /sum(d[d$ESTADO==1,]$PONDERA, na.rm=TRUE)
-                }                    
-      }
+  else {
+    indicator$ind<-sum(d[d$CAT_OCUP==2,]$PONDERA, na.rm=TRUE) /sum(d[d$ESTADO==1,]$PONDERA, na.rm=TRUE)
+  }
+  return(indicator)
 }
 
-indicator<-as.data.frame(indicator)
-colnames(indicator)<-c('year', 'trim', 'ind')
+df <- df %>%
+    dplyr::mutate(microdata=
+      purrr::pmap(list('year' = year,
+                       'trimester' = trimester,
+                       'wave' = wave,
+                       'type' = type),
+                  .f = get_data) 
+                  )
 
-indicator <- indicator %>%
-  mutate(fecha = lubridate::parse_date_time(paste(indicator$year, indicator$trim, sep='-'), "%y%q"))
 
 
-ggplot(data=indicator) +
-        geom_line(aes(x=fecha,y=ind), color='red') +
-        scale_y_continuous(limits=c(0,0.35)) + 
-        labs(y='Tasa de TCP / Ocupados')
+## ------------------------------------------------------------------------
+df %>% 
+  select(microdata) %>% 
+  map_df(.,bind_rows) %>% 
+  select(-indic) %>%
+  mutate(date=lubridate::parse_date_time(paste(year, trimester,sep='-'), '%y%q')) %>%
+  ggplot() + 
+    geom_line(aes(x=date,y=ind), color='blue') +
+    scale_y_continuous(limits=c(0,0.3)) +
+    labs(y='% TCP sobre ocupados')
 
+
+## ----eval=FALSE, include=FALSE-------------------------------------------
+#  years <- 2011:2018
+#  trims <- 1:4
+#  
+#  indicator<-matrix(nrow=length(years) * length(trims), ncol=3)
+#  i<-0
+#  for (y in years){
+#          for (t in trims){
+#                  i<-i+1
+#                  d<-get_microdata(year=y, trimester = t) %>%
+#                    organize_labels(.)
+#  
+#                  indicator[i,1]<-y
+#                  indicator[i,2]<-t
+#  
+#                  if (nrow(d) == 0){
+#                    indicator[i,3]<-NA
+#                  }
+#  
+#                  if (y==2007 & (t ==2 | t == 4)) {
+#                    indicator[i,3]<-sum(d[d$cat_ocup==2,]$pondera, na.rm=TRUE) /sum(d[d$estado==1,]$pondera, na.rm=TRUE)
+#                  }
+#  
+#                  else {
+#                  indicator[i,3]<-sum(d[d$CAT_OCUP==2,]$PONDERA, na.rm=TRUE) /sum(d[d$ESTADO==1,]$PONDERA, na.rm=TRUE)
+#                  }
+#        }
+#  }
+#  
+#  indicator<-as.data.frame(indicator)
+#  colnames(indicator)<-c('year', 'trim', 'ind')
+#  
+#  indicator <- indicator %>%
+#    mutate(fecha = lubridate::parse_date_time(paste(indicator$year, indicator$trim, sep='-'), "%y%q"))
+#  
+#  
+#  ggplot(data=indicator) +
+#          geom_line(aes(x=fecha,y=ind), color='red') +
+#          scale_y_continuous(limits=c(0,0.35)) +
+#          labs(y='Tasa de TCP / Ocupados')
+#  
 
 ## ----message=FALSE, warning=FALSE----------------------------------------
 bases <- dplyr::bind_rows(toybase_individual_2016_03,toybase_individual_2016_04)
@@ -113,5 +169,19 @@ lineas %>%
     geom_line(aes(x=periodo, y=valor, col=canasta))
 
 ## ------------------------------------------------------------------------
-#calculate_poverty(bases, basket=lineas)
+canastas_reg_example
+
+## ------------------------------------------------------------------------
+adulto_equivalente
+
+## ----warning=FALSE-------------------------------------------------------
+
+bases <- dplyr::bind_rows(toybase_individual_2016_03,toybase_individual_2016_04)
+base_pobreza <- calculate_poverty(base = bases, basket = canastas_reg_example,print_summary=TRUE)
+
+
+## ------------------------------------------------------------------------
+base_pobreza %>% 
+  select(CODUSU,ITF,region,adequi_hogar,CBA_hogar,CBT_hogar,situacion) %>% 
+  sample_n(10)
 
