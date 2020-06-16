6001bc8290950c48864270ed7e8cff6ab9fc5941
natsushok <natsumi.shokida@gmail.com>
2020-06-16 18:20:16 +0200

    agrego opcion de descarga de canastas regionales

--- R/get_poverty_lines.R
+++ R/get_poverty_lines.R
@@ -1,26 +1,37 @@
 #'Descarga de canasta basica alimentaria y canasta basica total
 #'@description
 #'Funcion que descarga la CBA y CBT a partir de abril 2016
+#'@param regional booleano, default = FALSE. Si es TRUE, descarga los datos de canastas regionales
+#'       que se utilizan para el calculo de pobreza. Si es FALSE, descarga la serie de GBA
+#'       provista por indec en https://www.indec.gob.ar/indec/web/Nivel4-Tema-4-43-149
 #'@details
 #'Disclaimer: El script no es un producto oficial de INDEC.
 #'
-#'La funcion descarga la serie provista por indec en
-#'https://www.indec.gob.ar/indec/web/Nivel4-Tema-4-43-149
-#'
 #'@examples
 #'
-#'canasta <- get_poverty_lines()
+#'canasta <- get_poverty_lines(regional = TRUE)
 #'
 #'@export
 
-get_poverty_lines <- function(){
+get_poverty_lines <- function(regional = FALSE){
 
-  temp = tempfile(fileext = ".xls")
+  if (regional){
+    temp = tempfile()
+    link <- 'https://github.com/holatam/data/raw/master/eph/canasta/canastas.rds'
+    utils::download.file(link,destfile=temp, mode='wb')
+
+    canasta <- readRDS(temp)
+    unlink(temp)
+
+  }
+  else{temp = tempfile(fileext = ".xls")
   dataURL <- "https://www.indec.gob.ar/ftp/cuadros/sociedad/serie_cba_cbt.xls"
   utils::download.file(dataURL, destfile=temp, mode='wb')
 
   suppressWarnings({canasta <- readxl::read_excel(temp, sheet =1, skip = 6,col_names = c('periodo', 'CBA', 'ICE', 'CBT'),
-                     col_types = c('date','numeric','numeric','numeric')) %>% stats::na.omit()})
-  unlink(temp)
+                                                  col_types = c('date','numeric','numeric','numeric')) %>% stats::na.omit()})
+  unlink(temp)}
+
   canasta
-  }
+
+}
--- tests/testthat/test-get_poverty_lines.R
+++ tests/testthat/test-get_poverty_lines.R
@@ -1,6 +1,6 @@
 context("test-get_poverty_lines")
 
-test_that("test canasta", {
+test_that("test GBA", {
   skip_if_offline()
   canasta <- get_poverty_lines()
   expect_equal(canasta$CBA[1], 1514.53)
@@ -8,3 +8,9 @@ test_that("test canasta", {
   expect_equal(canasta$CBT[1], 3663.66)
 })
 
+test_that("test regiones", {
+  skip_if_offline()
+  canasta <- get_poverty_lines(regional = TRUE)
+  expect_equal(canasta$CBA[1], 1224.64)
+  expect_equal(unique(canasta$region), c( "Cuyo" , "GBA", "Noreste", "Noroeste", "Pampeana", "Patagonia"))
+})
