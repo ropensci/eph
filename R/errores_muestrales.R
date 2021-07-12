af2c45b39a6dfd39d240b91ce00d4c773e406ec1
jgjuara <jg.juara@gmail.com>
2021-07-12 15:14:06 -0300

    agrego errores_muestrales 2003.03_2014.02 y 2014.03 en adelante con script en data_raw, .RDA en data creado con use_data y documentación roxygen

--- R/errores_muestrales.R
+++ R/errores_muestrales.R
@@ -1,11 +1,14 @@
 #' @title errores_muestrales
 #' @description Base con los errores muestrales para estimaciones puntuales en los 31 aglomerados urbanos.
-#' Estos errores rigen para la EPH continua desde 2014 tercer trimestre según documentación de INDEC:
+#' Estos errores rigen para la EPH continua desde 2003 segundo trimestre según documentación de INDEC:
 #' https://www.indec.gob.ar/ftp/cuadros/menusuperior/eph/EPH_errores_muestreo_3t2014.pdf
-#' @format A data frame with 935 rows and 4 variables:
+#' https://www.indec.gob.ar/ftp/cuadros/menusuperior/eph/EPH_errores_muestreo.pdf
+#' @format A data frame with 1687 rows and 5 variables:
 #' \describe{
-#'   \item{\code{aglomerado}}{character ---String con código numérico de aglomerado o con "Total" para el conjunto de los 31 aglomerados---}
-#'   \item{\code{x}}{double ---Estimación puntual para la cual se desea conocer el error muestral---}
+#'   \item{\code{codigo}}{character ---String con código numérico de aglomerado o con "Total" para el conjunto de los 31 aglomerados---}
+#'   \item{\code{aglomerado}}{character ---String con el nombre del aglomerado---}
+#'   \item{\code{periodo}}{character ---String indicando el periodo de EPH que corresponde, "2014.03" para datos de EPH a partir del tercer trimestre 2014, o "2003.03_2014.02" para datos anteriores---}"
+#'   \item{\code{x}}{double ---Estimación puntual de individuos para la cual se desea conocer el error muestral---}
 #'   \item{\code{ds}}{double ---Desvío Estándar para la estimacióm puntual en el aglomerado correspondiente---}
 #'   \item{\code{cv}}{double ---Coeficiente de Variación para la estimacióm puntual en el aglomerado correspondiente---}
 #'}
